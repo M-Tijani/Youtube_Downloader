8fec0a52d01ad77ba497ad81bd6f81f27d3a2b6a
AmineNaboulsi <naboulsiiamine@gmail.com>
2024-02-18 15:40:09 +0100

    Client part : get header api page (how to use)

--- Client/src/Pages/Howtouse.tsx
+++ Client/src/Pages/Howtouse.tsx
@@ -1,5 +1,69 @@
+import { useEffect, useState } from 'react';
+import "../Css/howtouse.css"
+
+interface headermodel {
+  header: {
+    _id: string;
+    title: string;
+    subtitle: string;
+    first_description: string;
+    second_description: string;
+  };
+}
+
 function Howtouse() {
-  return <div>Howtouse</div>;
+  const [list_header, setheader] = useState<headermodel | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/api');
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+
+        const result = await response.json();
+        console.log(result);
+
+        setheader(result);
+      } catch (error) {
+        console.log('Error fetching data:');
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className='howtouse'>
+      <div className='howtouseheader'>
+
+        {list_header ? (
+          list_header.header.map((itm, i) => (
+            <>
+              <h1 className='title'>1. {itm.title}</h1>
+              <h1 className='subtitle'>{itm.subtitle}</h1>
+              <h1 className='desc'>{itm.first_description}</h1>
+              
+              {itm.steps.map((step , i)=>(
+                <>
+                <h2>Step {i+1} :</h2>
+                <ul>
+                  <li key={i}>{step}</li>
+                </ul>
+                </>
+                
+              ))}
+              
+              <h1 className='desc'>{itm.second_description}</h1>
+            </>
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Howtouse;
