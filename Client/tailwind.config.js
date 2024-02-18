141f0fec3174e0d4c773e82880edd7bd5007125a
M-Tijani <150944603+M-Tijani@users.noreply.github.com>
2024-02-18 04:11:21 -1200

    Client : Hero Download Form + Contact From not working yet

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -6,6 +6,10 @@ export default {
       colors: {
         50: "#DCDCDC",
         100: "#3742FA",
+        101: "#252cdd",
+        102: "#c0d5ff",
+        200: "#F2F2F2",
+        300: "#D9D9D9",
       },
       fontFamily: {
         Inter: ["Inter", "ui-sans-serif", "system-ui"],
