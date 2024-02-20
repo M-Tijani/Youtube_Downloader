ed5794d3030e397f64100d899362e4dea991b0e3
M-Tijani <150944603+M-Tijani@users.noreply.github.com>
2024-02-19 21:43:21 -1200

    Client: Trying To push

--- Client/src/Pages/Footer.tsx
+++ Client/src/Pages/Footer.tsx
@@ -1,9 +1,19 @@
+import Logo from "/src/assets/Tubeload White.png";
+import { Link } from "react-router-dom";
 function Footer() {
   return (
     <>
-      <main className="fixed bottom-0 right-0 left-0 bg-100">
-        <div className="flex items-center justify-between px-4 py-4 bg-100 text-white">
-          <h1>hello</h1>
+      <main className="mt-auto bottom-0 right-0 left-0 bg-100">
+        <div className="grid grid-col-1 justify-items-center text-justify gap-2 px-4 py-4 bg-100 text-white">
+          <Link className="text-2xl font-semibold" to="/">
+            <img src={Logo} alt="" />
+          </Link>
+          <p>
+            With Tubeload's YouTube converter, you can easily transform your
+            favorite YouTube videos into high-quality mp3 or mp4 files. Whether
+            you want to enjoy music on the go or save videos for offline
+            viewing, Tubeload's converter has got you covered.
+          </p>
         </div>
       </main>
     </>
--- Client/src/components/main/Downloader.tsx
+++ Client/src/components/main/Downloader.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { tsParticles } from "@tsparticles/engine";
+
 function Downloader() {
   return (
     <>
