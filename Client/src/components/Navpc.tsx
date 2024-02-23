e6bdee26e2534a448a61abfb822b6ac0a761ae7b
AmineNaboulsi <naboulsiiamine@gmail.com>
2024-02-23 17:25:23 +0100

    Client : Sign in with google Done

--- Client/src/components/Navpc.tsx
+++ Client/src/components/Navpc.tsx
@@ -1,6 +1,63 @@
 import { NavLink, Link } from "react-router-dom";
 import Logo from "/src/assets/Tubeload White.png";
+import { GoogleLogin, GoogleOAuthProvider, GoogleLogout, googleLogout } from '@react-oauth/google';
+
+import { jwtDecode } from "jwt-decode";
+import { useEffect, useState } from "react";
+import "../Css/Navpc.css"
+import { IoIosArrowDown } from "react-icons/io";
+import Cookies from 'js-cookie';
+
 function Navpc() {
+
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [userInfo, setUserInfo] = useState({ name: '', email: '', profileImage: '' });
+
+  useEffect(() => {
+    const user = Cookies.get('credentialtxt');
+    if (user !== undefined) {
+      setLoggedIn(true);
+      getcardprofil(user);
+    }
+    else
+      setLoggedIn(false);
+
+  });
+  const openmenucard = () => {
+    const card = document.querySelector(".menupanel");
+    const iconmenu = document.querySelector(".IoIosArrowDown");
+    if (card.style.display == 'flex'){
+      card.style.display = 'none'
+      iconmenu.style.rotate = '180deg'
+    }
+    else{
+      iconmenu.style.rotate = '0deg'
+      card.style.display = 'flex'
+    }
+      
+
+  }
+  const getcardprofil = (credentialtxt) => {
+    const decoded = jwtDecode(credentialtxt);
+    if (decoded) {
+      const userName = decoded?.name || 'Unknown';
+      const userEmail = decoded?.email || 'Unknown';
+      const userProfileImage = decoded?.picture || '';
+
+      setUserInfo({ name: userName, email: userEmail, profileImage: userProfileImage });
+      setLoggedIn(true);
+      Cookies.set('credentialtxt', credentialtxt);
+      
+    }
+
+  }
+  const GoogleLoginSuccess = (credentialResponse) => {
+    const token = credentialResponse?.credential;
+    getcardprofil(token);
+    setCookie("credentialtxt", token);
+
+  };
+
   return (
     <>
       <main className="w-full">
@@ -23,17 +80,48 @@ function Navpc() {
             >
               CONTACT US
             </NavLink>
-            <NavLink
-              className="text-white text_style aria-[current=page]:text-600"
-              to="/history"
-            >
-              HISTORY
-            </NavLink>
           </div>
-          <div className="flex gap-4">
-            <Link className="button_style py-2 px-6" to="/Login-or-register">
-              CONTINUE GOOGLE
-            </Link>
+          <div className="">
+
+            <GoogleOAuthProvider
+              clientId="347025856432-g5s8g8750kdm1klmfhdeqt4hpi8tun23.apps.googleusercontent.com"
+              cookiePolicy={'single_host_origin'}
+            >
+              {loggedIn ? (
+                <>
+
+                  <div className="cardprofil" onClick={openmenucard}>
+                    <img className="profilimg" src={userInfo.profileImage} alt="User Profile" />
+                    <div className="detailsinfo">
+                      <h2>{userInfo.name}</h2>
+                      <p>{userInfo.email}</p>
+                    </div>
+                    <IoIosArrowDown className="IoIosArrowDown" style={{ fontSize: '22px', color: '#f8f8f8', marginRight: "5px" }} />
+                    <div className="menupanel">
+                      <NavLink to='/History' >
+                        <div className="history">History</div>
+                      </NavLink>
+                      <div className="logout" onClick={()=>{
+                        googleLogout();
+                        Cookies.remove('credentialtxt');
+                        setLoggedIn(false);
+                      }}>Log out</div>
+                    </div>
+                  </div>
+                </>
+
+              ) : (
+                <GoogleLogin
+                  onSuccess={GoogleLoginSuccess}
+                  onError={() => {
+                    console.log('Login Failed');
+                  }}
+                />
+
+              )}
+
+
+            </GoogleOAuthProvider>
           </div>
         </div>
       </main>
